3e3763ec854c5d78f21f4f8ab78d02cea48d4d94
dev <furtif86@live.fr>
2019-09-21 16:33:07 +0200

    <init>: init  first componant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMON } from './mock-pokemons';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pokemon-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng6-pokemon';
+export class AppComponent implements OnInit {
+
+
+  private pokemons: Pokemon[];
+  private title: string = "Liste des pokémons";
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.pokemons = POKEMON;
+  }
+
+  selectPokemon(pokemon: Pokemon) {
+    alert("Vous avez cliqué sur: " + pokemon.name);
+  }
 }
